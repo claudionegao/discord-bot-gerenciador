07c2b33b63db4d6e0a3d5d15304476318befb21b
Claudio Costa <costa.pederneschi@gmail.com>
2022-10-16 11:28:06 -0300

    testes

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,13 @@
-import { Api } from "./api";
+import { Api } from "./api.js";
+const loginBtn = document.getElementsByTagName('button')[0]
+loginBtn.addEventListener('click',(a)=>{
+    a.preventDefault()
+    login()
+})
+function login() {
+    const obj = {
+        email:document.getElementById('email').value,
+        password:document.getElementById('password').value
+    }
+    console.log(JSON.parse(Api.reqBody(obj,login)))
+}
