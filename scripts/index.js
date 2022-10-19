cfb8c036305dc6b9e8a42e5a22fa800464fb47b6
Claudio Costa <costa.pederneschi@gmail.com>
2022-10-19 20:40:51 -0300

    Reset

--- scripts/index.js
+++ scripts/index.js
@@ -1,18 +1,1 @@
 import { Api } from "./api.js";
-const loginBtn = document.getElementsByTagName('button')[0]
-loginBtn.addEventListener('click',(a)=>{
-    a.preventDefault()
-    login()
-})
- async function login() {
-    const username = document.getElementById('username').value
-    const password = document.getElementById('password').value
-    const obj = {
-        username:username,
-        password:password
-    }
-    const resp = await Api.reqBody(obj,'login')
-    console.log(resp)
-    localStorage.setItem('token',resp._id)
-    location.reload()
-}
