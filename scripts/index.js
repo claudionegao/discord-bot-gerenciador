6f6aecb153b6f9be2be903644d97efdd0b36ab97
Claudio Costa <costa.pederneschi@gmail.com>
2022-10-19 22:08:16 -0300

    TESTANDO

--- scripts/index.js
+++ scripts/index.js
@@ -1,1 +1,18 @@
 import { Api } from "./api.js";
+let touchstartX = 0
+let touchendX = 0
+    
+function checkDirection() {
+const text = document.getElementsByClassName('span')[0]
+  if (touchendX < touchstartX) text.textContent = 'swiped left!'
+  if (touchendX > touchstartX) text.textContent = 'swiped right!'
+}
+
+document.addEventListener('touchstart', e => {
+  touchstartX = e.changedTouches[0].screenX
+})
+
+document.addEventListener('touchend', e => {
+  touchendX = e.changedTouches[0].screenX
+  checkDirection()
+})
